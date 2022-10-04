13e3484c0dfdd12aaae275a2964325051f77332e
Tamas Nemes <t.nemes@axes4.com>
2022-10-04 16:43:05 +0200

    Replace list styling with ListTile widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -32,7 +32,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 500,
+      height: 508,
       child: transactions.isEmpty
           ? Column(
               children: [
@@ -52,6 +52,75 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (context, index) {
                 return Card(
+                  elevation: 5,
+                  margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: ListTile(
+                      leading: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color:
+                              categoryIcons[transactions[index].category]!['theme']
+                                  as Color,
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 3,
+                          ),
+                        ),
+                        child: Stack(
+                          children: [
+                            Icon(
+                              categoryIcons[transactions[index].category]![
+                                  'icon_fill'] as IconData,
+                              color: Colors.white,
+                              size: 50,
+                            ),
+                            Icon(
+                              categoryIcons[transactions[index].category]![
+                                  'icon_outline'] as IconData,
+                              color: Colors.black,
+                              size: 50,
+                            ),
+                          ],
+                        ),
+                      ),
+                      title: Text(
+                        transactions[index].desc,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 17,
+                        ),
+                      ),
+                      subtitle: Text(
+                        DateFormat('d. MMMM y').format(transactions[index].date),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      trailing: Text(
+                        "${transactions[index].type == TransactionType.expense ? "- " : ""}${transactions[index].amount.toStringAsFixed(2)} ${currencySymbols[currentCurrency]}",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 16,
+                          color: transactions[index].type == TransactionType.expense
+                              ? const Color.fromARGB(255, 230, 126, 164)
+                              : const Color.fromARGB(255, 180, 211, 137),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
+            ),
+    );
+  }
+}
+
+/*return Card(
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -133,10 +202,4 @@ class TransactionList extends StatelessWidget {
                       )
                     ],
                   ),
-                );
-              },
-              itemCount: transactions.length,
-            ),
-    );
-  }
-}
+                );*/
